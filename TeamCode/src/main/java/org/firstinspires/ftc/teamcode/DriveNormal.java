5e564e2066ac7cbc5222c5291cd27fa8c52965e2
sgu-101 <112212519+sgu-101@users.noreply.github.com>
2024-02-09 18:43:58 -0500

    macros
    aaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveNormal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveNormal.java
@@ -14,9 +14,8 @@ public class DriveNormal extends LinearOpMode {
 
     private DcMotorEx FR, FL, BL, BR;
     private double FRP, FLP, BLP, BRP;
-    private Servo claw, claw2, drone, Larm, Rarm;
+    private Servo claw, claw2, drone, Larm, Rarm, wrist;
 
-    private CRServo wrist;
     double MIN_POSITION = 0, MAX_POSITION = 1;
     double contPower;
     @Override
@@ -32,20 +31,22 @@ public class DriveNormal extends LinearOpMode {
 //        arm = hardwareMap.get(CRServo.class, "arm");
         //arm2 = hardwareMap.get(CRServo.class, "arm2");
         drone = hardwareMap.get(Servo.class,"drone");
-        wrist = hardwareMap.get(CRServo.class, "wrist");
+        wrist = hardwareMap.get(Servo.class, "wrist");
 
         double dronePos =0.65;
         FR.setDirection(DcMotorEx.Direction.FORWARD);
         BR.setDirection(DcMotorEx.Direction.FORWARD);
         double speedDivide = 1;
         double clawPosition;
+        double wristPosition;
         BL.setDirection(DcMotorEx.Direction.REVERSE);//switched from BR TO BL
         FL.setDirection(DcMotorEx.Direction.REVERSE);//switched from FR TO FL
         Rarm.setDirection(Servo.Direction.REVERSE);
 
         waitForStart();
 
-        clawPosition = .5;
+        clawPosition = 1;
+        wristPosition = 0.73;
 
         while(opModeIsActive()) {
             double y = 0.8*(Math.pow(-gamepad1.left_stick_y,2))*Math.signum(-gamepad1.left_stick_y); //y value is inverted
@@ -60,20 +61,12 @@ public class DriveNormal extends LinearOpMode {
 
 
             //claw
-            if ((gamepad1.right_trigger > 0) && clawPosition > MIN_POSITION) clawPosition -= .01;
-            if ((gamepad1.left_trigger > 0) && clawPosition < MAX_POSITION) clawPosition += .01;
+            if (gamepad1.right_trigger > 0.2) clawPosition = 1;
+            if (gamepad1.left_trigger > 0.2) clawPosition = 0.7;
 
 
             //wrist
-
-            if (gamepad1.dpad_left){
-                wrist.setPower(0.2);
-            } else if (gamepad1.dpad_right){
-                wrist.setPower(-0.2);
-            } else{
-                wrist.setPower(0.0);
-            }
-
+            //0.78,0.729(floor)
             //drone
             if (gamepad1.x) {
                 dronePos -= 0.01;
@@ -81,11 +74,13 @@ public class DriveNormal extends LinearOpMode {
 
             //slides
             if (gamepad1.right_bumper){
-                Larm.setPosition(0.7);
-                Rarm.setPosition(0.7);
+                Larm.setPosition(0.9);
+                Rarm.setPosition(0.9);
+                wristPosition =0.81;
             } else if (gamepad1.left_bumper) {
                 Rarm.setPosition(0);
                 Larm.setPosition(0);
+                wristPosition = 0.73;
             }
 
             double frontLeftSpd=(y+x+rx)/speedDivide; //y+x+rx
@@ -110,13 +105,15 @@ public class DriveNormal extends LinearOpMode {
             BR.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
             FR.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
-            claw.setPosition(Range.clip(clawPosition, MIN_POSITION, MAX_POSITION));
+            claw.setPosition(Range.clip(clawPosition-0.1, MIN_POSITION, MAX_POSITION));
             claw2.setPosition(Range.clip(1-clawPosition, MIN_POSITION, MAX_POSITION));
             drone.setPosition(Range.clip(dronePos, MIN_POSITION,MAX_POSITION));
+            wrist.setPosition(Range.clip(wristPosition, MIN_POSITION, MAX_POSITION));
             //arm2.setPower(-contPower);
 
             telemetry.addData("state",drone.getPosition());
-            telemetry.addData("right trig",gamepad1.right_trigger);
+//            telemetry.addData("right trig",gamepad1.right_trigger);
+            telemetry.addData("wrist position", wrist.getPosition());
             telemetry.update();
         }
 
